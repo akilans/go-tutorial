1cea268c3d0d32ae25d436401247613ed3ea55e2
Akilan <akil.dove@gmail.com>
2020-10-20 01:21:08 +0100

    switch example added

--- 07-condition/main.go
+++ 07-condition/main.go
@@ -3,25 +3,54 @@ package main
 import "fmt"
 
 // Grade system
-var mark int = 80
+var mark int = 101
+
+var akilanDetails = map[string]string{
+	"name":     "Akilan",
+	"location": "London",
+}
 
 func main() {
-	if mark < 0 || mark > 100 {
-		fmt.Print("Please enter mark between 0 - 100 ")
-	} else if mark >= 0 && mark < 35 {
-		fmt.Print("Failed!!!")
-	} else if mark >= 35 && mark <= 50 {
-		fmt.Print("Average!!!")
-	} else if mark > 50 && mark <= 75 {
-		fmt.Print("Good!!!")
-	} else if mark > 75 && mark <= 90 {
-		fmt.Print("Very Good!!!")
-	} else if mark > 90 && mark <= 100 {
-		fmt.Print("Awesome!!!")
-	} else {
-		fmt.Print("No Idea!!!")
+
+	var keyName string = "name"
+
+	// Key exists check
+	if val, ok := akilanDetails[keyName]; ok {
+		fmt.Println("Key " + keyName + " Exists. & the value is " + val)
 	}
 
-	fmt.Print("\n")
+	if mark < 0 || mark > 100 {
+		fmt.Println("Please enter mark between 0 - 100!!!")
+	} else if mark >= 0 && mark < 35 {
+		fmt.Println("Failed!!!")
+	} else if mark >= 35 && mark <= 50 {
+		fmt.Println("Average!!!")
+	} else if mark > 50 && mark <= 75 {
+		fmt.Println("Good!!!")
+	} else if mark > 75 && mark <= 90 {
+		fmt.Println("Very Good!!!")
+	} else if mark > 90 && mark <= 100 {
+		fmt.Println("Awesome!!!")
+	} else {
+		fmt.Println("No Idea!!!")
+	}
+
+	switch {
+	case mark < 0 || mark > 100:
+		fmt.Println("Please enter mark between 0 - 100!!!")
+	case mark >= 0 && mark < 35:
+		fmt.Println("Failed!!!")
+	case mark >= 35 && mark <= 50:
+		fmt.Println("Average!!!")
+	case mark > 50 && mark <= 75:
+		fmt.Println("Good!!!")
+	case mark > 75 && mark <= 90:
+		fmt.Println("Very Good!!!")
+	case mark > 90 && mark <= 100:
+		fmt.Println("Awesome!!!")
+	default:
+		fmt.Println("No Idea!!!")
+
+	}
 
 }
